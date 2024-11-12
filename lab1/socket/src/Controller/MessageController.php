4eddc27e9df1c3b4c2bbfd3bc0921e2d82d64d2a
WhtsPoint <whtspoint@gmail.com>
2024-11-12 23:39:57 +0200

    Progress

--- lab1/socket/src/Controller/MessageController.php
+++ lab1/socket/src/Controller/MessageController.php
@@ -2,10 +2,40 @@
 
 namespace App\Controller;
 
+use App\Dto\ActionDto;
+use OpenSwoole\Http\Request;
+use OpenSwoole\WebSocket\Server;
+
 class MessageController
 {
-    public function runActionByMessage(array $message): void
-    {
+    public function __construct(
+        private readonly array $actions
+    ) {
+    }
 
+    public function runActionByMessage(Server $server, Request $request): void
+    {
+       $content = $request->getContent();
+       $body = is_string($content) ? json_decode($content, true) : false;
+
+       if ($body === false || is_string($body['type']) === false || is_array($body['message']) === false) {
+           $server->push($request->fd, json_encode(['error' => 'Invalid payload format']));
+
+           return;
+       }
+
+       if (isset($this->actions[$body['type']]) === false) {
+           $server->push($request->fd, json_encode(['error' => 'Invalid message type']));
+
+           return;
+       }
+
+       $this->actions[$body['type']]->do(
+           new ActionDto(
+               $body['type'],
+               $body['message'],
+               $request->fd
+           )
+       );
     }
 }
